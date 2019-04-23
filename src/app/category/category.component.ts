2d12be6c09d3a33817f2708712d8dee53af4bbc0
Shiv <mishra.shiv68@gmail.com>
2019-04-23 11:35:37 +0530

    Commiting slider under line styling

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, Output,EventEmitter, ViewChild } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter, ViewChild, AfterViewInit } from '@angular/core';
 import {CategoryService} from './category.service'
 import { Category } from './category';
 import { SubCategory } from './sub-category';
 import { NgbCarouselConfig, NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import {Slider} from './slider';
 
 
-
-
-
+declare var custom:any;
 
 
 @Component({
@@ -16,12 +15,22 @@ import { NgbCarouselConfig, NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./category.component.css'],
   providers:[CategoryService,Category,SubCategory, NgbCarouselConfig,NgbCarousel]
 })
-export class CategoryComponent implements OnInit {
+export class CategoryComponent implements OnInit,AfterViewInit {
+  ngAfterViewInit(): void {
+    custom(0,1);
+  }
  
   @ViewChild('myCarousel')  myCarousel:NgbCarousel
   public categoryList:Category[];
   public categoryListAll:Category[];
   public sub_categoryList:SubCategory[];
+  public sliderList:Slider[] = [
+    {id: '55',name:'Summer Sale',imageLink:'images\\summer-sale.jpg'},
+    {id: '56',name:'Gym Shaker',imageLink:'images\\gym-shaker.jpg'},
+    {id: '57',name:'Cleaners Tissue',imageLink:'images\\cleaners-tissue.jpg'},
+  
+  ];
+  
   constructor(private categoryService: CategoryService,config: NgbCarouselConfig, myCarousel:NgbCarousel) {
     config.interval = 9000;
     config.wrap = true;
@@ -30,12 +39,17 @@ export class CategoryComponent implements OnInit {
     config.showNavigationIndicators=false;
    this.myCarousel=myCarousel;
    }
+   onSlideChanged(ev)
+   {
+     
+custom(ev.current,ev.prev);
 
+   }
 
    
   ngOnInit() {
     this.categoryListAll=this.categoriesOnload();
-   
+   // custom(0,1);
     
 
   }
@@ -72,7 +86,7 @@ tellMeTheSlide(x)
   
  // alert("Active==" +this.myCarousel.activeId)
   this.myCarousel.select(x);
-
+  
 }
 
   
